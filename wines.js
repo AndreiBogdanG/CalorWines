4a5bc2dcbd11d5cfea16b951cdd7cd6979504163
Andrei Bogdan G <alcatraz0405@yahoo.com>
2026-04-10 06:24:14 +0300

    Update prices

--- wines.js
+++ wines.js
@@ -32,7 +32,7 @@ const pipe4 = {
     bottleVolume: "750 ml",
     alcoholContent: "12.5%",
     qualityClassification: "C.D.O. - Murfatlar vineyard",
-    priceWithoutVAT: "EUR 12",
+    priceWithoutVAT: "EUR 15",
   },
   fullDescription: `It displays a delicate, bright peach-rose color, while on the nose it reveals a fresh, lively, and generous aromatic combination, offering well-defined fruity sensations of raspberries, white flowers, strawberries, and red apples, with above-average intensity and a friendly character.<br><br>
 
@@ -61,7 +61,7 @@ const pipe12 = {
     bottleVolume: "75 cl",
     alcoholContent: "12.9%",
     qualityClassification: "",
-    priceWithoutVAT: "EUR 14",
+    priceWithoutVAT: "EUR 19",
   },
   fullDescription: `It has a delicate onion-skin color and displays intense, enticing aromas of red apples, raspberries, and strawberries, complemented by secondary vegetal and citrus notes, with lightly spiced impressions. The palate is fruity and well-defined, featuring ripe red apple tones, balanced and refreshing, supported by elegant and precise acidity.<br><br>
 
@@ -91,7 +91,7 @@ const pipe3 = {
     bottleVolume: "75 cl",
     alcoholContent: "12.6%",
     qualityClassification: "",
-    priceWithoutVAT: "EUR 12",
+    priceWithoutVAT: "EUR 15",
   },
   fullDescription: `It expresses itself clearly and precisely, in well-defined tones of wildflowers, apricot blossoms, green apples and pears, and lime, supported in the background by delicate, slightly sweet notes of pollen. Easy to drink and approachable, it quickly makes itself understood thanks to the vibrant, friendly, and fresh sensations it offers.<br><br>
  Its juicy yet crisp palate opens with notes of green apples, followed by a second phase balanced by acidic-vegetal hints of apricots and unripe cherry plums. On the finish, it leaves a lightly grainy texture due to its persistent acidity, while subtle spicy-alcoholic sensations lend it freshness and volume.
@@ -121,7 +121,7 @@ const pipe1 = {
     bottleVolume: "75 cl",
     alcoholContent: "13.5%",
     qualityClassification: "",
-    priceWithoutVAT: "EUR 16.5",
+    priceWithoutVAT: "EUR 23",
   },
   fullDescription: `A dry, barrel-aged white wine that surprises with its elegant body, good acidity, and notes of oriental spices on the finish.<br><br>
 The grapes selected for Sole Fetească Regală come predominantly from the terraces of Valea Săldăbaș with western exposure. The longer ripening period in these parcels allows the grapes to develop structure and complex aromatic nuances. The wine is pressed, clarified, and fermented at low temperatures to preserve the primary floral and fruity aromas, after which it undergoes 3 months of barrique aging, allowing the integration of the polyphenols typical of this variety, resulting in a body of exemplary finesse.
@@ -150,7 +150,7 @@ const pipe5 = {
     bottleVolume: "75 cl",
     alcoholContent: "13%",
     qualityClassification: "",
-    priceWithoutVAT: "EUR 16.5",
+    priceWithoutVAT: "EUR 20",
   },
   fullDescription: `A dry rosé wine with a silky, light texture, accompanied by intense aromas and a supple body, making this balanced, fresh, and refreshing wine a true pleasure on warm summer days.<br><br>
 The wine is defined by intense, bright raspberry-pink hues. On the nose, it reveals fruity aromas dominated by raspberry and ripe blackberries, along with floral notes reminiscent of peonies. Sole Rosé is a dry wine with vibrant acidity, accompanied by medium to high tannins. The finish is intense, herbal, and slightly spicy. Its silky, light texture, together with its intense aromas and supple body, make this wine balanced, fresh, and invigorating.
@@ -179,7 +179,7 @@ const pipe6 = {
     bottleVolume: "75 cl",
     alcoholContent: "13%",
     qualityClassification: "",
-    priceWithoutVAT: "EUR 13",
+    priceWithoutVAT: "EUR 15",
   },
   fullDescription: `“I am refreshing, lively, and expressive, like a melody with a vibrant rhythm that instantly carries your steps onto the dance floor. I invite you to celebrate with every tasting. Are you ready to get to know me?” <br><br>
 I am a clear wine with salmon-pink hues, defined by intense aromas of red fruits: strawberries, raspberries, and sour cherry jam. In the background, you will notice aromatic notes of blood orange, vine blossom, and rose water. I am a perfectly balanced rosé wine, with a delicate body and an aromatic finish. You will discover me gradually, through familiar notes and flavors, as a versatile culinary companion, ideal for pairing with a wide variety of dishes.
@@ -208,7 +208,7 @@ const pipe7 = {
     bottleVolume: "75 cl",
     alcoholContent: "13.2%",
     qualityClassification: "",
-    priceWithoutVAT: "EUR 16",
+    priceWithoutVAT: "EUR 20",
   },
   fullDescription: `The blend of the Rusalcă Albă variety is a complete white wine that combines the fruitiness and character of Chardonnay with the finesse and aroma of Sauvignon, complemented by the richness and vibrancy of Riesling, all set on the well-balanced structure of Pinot Gris.<br><br>
 It is a lively, fresh, and harmonious wine, with an intense and pleasant aroma that intertwines the delicate scent of spring blossoms with the mineral notes characteristic of the region.
@@ -237,7 +237,7 @@ const pipe8 = {
     bottleVolume: "75 cl",
     alcoholContent: "14.5%",
     qualityClassification: "",
-    priceWithoutVAT: "EUR 13",
+    priceWithoutVAT: "EUR 17",
   },
   fullDescription: `A well-balanced and elegant semi-dry wine, with intense floral aromas of acacia blossom and linden honey, delicate and pleasant, featuring a harmonious and well-integrated structure. The 10 g/L of residual sugar were intentionally retained to enhance the intense and appealing floral notes, while also lending the wine a graceful character. <br><br>
 Direct and approachable, with complex nuances of pineapple, mango, acacia flowers, and citrus, it is a friendly wine, suitable for any occasion.
@@ -266,7 +266,7 @@ const pipe9 = {
     bottleVolume: "75 cl",
     alcoholContent: "13%",
     qualityClassification: "",
-    priceWithoutVAT: "EUR 14",
+    priceWithoutVAT: "EUR 18",
   },
   fullDescription: `Jiana, the new memorable rosé from Crama Oprișor, weaves together contrasts and synergies. The refined intensity of its pale pink hue is complemented by the freshness of enticing aromas of white cherries, peaches, and mango. <br><br>
 Well-balanced acidity, seamlessly integrated into an elegant, slightly creamy structure, along with retronasal sensations of May cherries, spices, and vanilla, moderate alcohol, and a long finish, make this wine a perfect companion for any summer day.
@@ -295,7 +295,7 @@ const pipe10 = {
     bottleVolume: "75 cl",
     alcoholContent: "13.5%",
     qualityClassification: "",
-    priceWithoutVAT: "EUR 14",
+    priceWithoutVAT: "EUR 15",
   },
   fullDescription: `Aromatic from the very first nose, it reveals fresh notes of May cherries and strawberries, nuanced by citrus and vegetal hints. On the palate, it opens with sweet sensations, then evolves under the influence of an acidity typical of young wines, leading to a finish that is fresh, lively, elegant, and pleasant.`,
 };
@@ -322,7 +322,7 @@ const pipe11 = {
     bottleVolume: "75 cl",
     alcoholContent: "14.5%",
     qualityClassification: "",
-    priceWithoutVAT: "EUR 13",
+    priceWithoutVAT: "EUR 18",
   },
   fullDescription: `Produced from carefully selected grapes, carefully fermented, and aged in oak barrels, this wine is passionate, harmonious, intense, powerful, and elegant. It was macerated just the right amount to give it strength, aged long enough to impart velvety smoothness, and crafted to be enjoyed either immediately or in two to three years. <br><br>
 Its complexity pleasantly surprises after the first olfactory impression, evoking cranberries and cocoa. The elegant attack is followed by a long, enveloping palate that carries you from candied fruits to dark chocolate.
@@ -351,7 +351,7 @@ const pipe2 = {
     bottleVolume: "75 cl",
     alcoholContent: "14%",
     qualityClassification: "",
-    priceWithoutVAT: "EUR 12",
+    priceWithoutVAT: "EUR 15",
   },
   fullDescription: `The flagship wine of the Caloian range, Fetească Neagră quickly won over its admirers. It recalls the erotic, postmodern surprise of a lady who, at a black-and-white ball, majestically descends the marble staircase in a red flamenco dress. A wine that never goes unnoticed, is never left uncommented, and inevitably lingers in memory.<br><br>
 Although it drinks easily, it is not a simple wine, as its friendly roundness—dominated by dried plums, cherries, and black sour cherries—is merely a “setting” for sensations of truffles and cocoa, slightly intoxicated by a drop of fresh blood, immersed in seductive oak aromas.
@@ -380,7 +380,7 @@ const pipe13 = {
     bottleVolume: "75 cl",
     alcoholContent: "14%",
     qualityClassification: "",
-    priceWithoutVAT: "EUR 17",
+    priceWithoutVAT: "EUR 20",
   },
   fullDescription: `In Dragaica Rosie, the elegance of Feteasca Neagra blends with the strength of Cabernet Sauvignon, the velvety texture of Merlot, and the spiciness of Shiraz. It is balanced and harmonious, with intense and pleasant nuances that reveal aromas of dried plums, blackcurrants, lightly toasted wood, and milk chocolate.<br><br>
 The resulting wine is intense and well-balanced, with powerful notes of blackcurrant, dried plum, cocoa, and lightly toasted oak. The well-ripened tannins are balanced by a pleasant acidity, well-integrated alcohol, and a full body with a long finish. <br><br>
@@ -410,7 +410,7 @@ const pipe14 = {
     bottleVolume: "75 cl",
     alcoholContent: "14.9%",
     qualityClassification: "",
-    priceWithoutVAT: "EUR 32",
+    priceWithoutVAT: "EUR 45",
   },
   fullDescription: `Noted for its intense color and persistent aromas of candied red fruits enveloped in dark chocolate, Crama Oprișor’s Fetească Neagră introduces itself with an elegant first impression. The sensation continues with a long, enveloping finish, marked by the slightly assertive character of powerful yet well-ripened tannins, perfectly rounded during the extended aging period in oak barrels. <br><br>
 It is at once a powerful yet voluptuous wine, generous in its energy, yet well-structured and balanced—perfect for an elaborate and sophisticated gastronomic evening or for a personal moment of reflection. <br><br>
